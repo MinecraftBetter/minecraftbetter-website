c15ab8c738846442fee43cfff2dfd1475e4bcfe8
EvanG <evan_g@orange.fr>
2022-06-23 17:03:54 +0200

    Initial website

--- index.php
+++ index.php
@@ -3,11 +3,45 @@
  * Made by Evan Galli *
  **********************/
 ?>
-<html>
+
+<!DOCTYPE>
+<html lang="en">
     <head>
         <title>Minecraft Better</title>
+        <style>
+            body {
+                height: 100%;
+                margin: 0;
+                display: table;
+                width: 100%;
+                background: #1f1f1f;
+            }
+
+            .centered {
+                color: white;
+                text-align: center;
+                vertical-align: middle;
+                display: table-cell;
+            }
+
+            .btn {
+                padding: 0.75em 1em;
+                background: red;
+                border-radius: 1em;
+                display: inline-block;
+                color: white;
+                text-decoration: none;
+            }
+        </style>
     </head>
     <body>
-
+        <small style="color:#1f1f1f; position: absolute; bottom: 0; right: 0; z-index:1;">Evan est aussi passé par là</small>
+        <div class="centered">
+            <h1 style="margin: 0">Minecraft Better</h1>
+            <small>Un serveur minecraft</small>
+            <br/>
+            <br/>
+            <a href="https://github.com/MinecraftBetter/launcher" class="btn">Commencer à jouer</a>
+        </div>
     </body>
 </html>
